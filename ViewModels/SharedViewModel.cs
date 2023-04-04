a73348fa9d144be63c2cbc0fd270aa809cf0f40e
ZangetsuTG <129702732+ZangetsuTG@users.noreply.github.com>
2023-04-04 13:20:57 +0900

    Add files via upload

---
+++ ViewModels/SharedViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.ViewModels
+{
+    public class SharedViewModel : BaseViewModel
+    {
+        private User user;
+        public User User
+        {
+            get
+            {
+                return AppData.User;
+            }
+        }
+    }
+}
